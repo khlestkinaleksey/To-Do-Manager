f75fb3706721c661f6ea76ca1c01e3fe3193d872
khlestkinaleksey <khlestkinvarshaveraleksey@gmail.com>
2023-08-04 04:25:22 +0300

    1. Added TaskCell

--- To-Do Manager/View/Cells/TaskCell.swift
+++ To-Do Manager/View/Cells/TaskCell.swift
@@ -8,16 +8,7 @@
 import UIKit
 
 class TaskCell: UITableViewCell {
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
+    
+    @IBOutlet var symbol: UILabel!
+    @IBOutlet var title: UILabel!
 }
