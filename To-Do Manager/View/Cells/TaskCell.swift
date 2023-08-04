5df040195108b2bfe9c261ff682c7d9e419d0696
khlestkinaleksey <khlestkinvarshaveraleksey@gmail.com>
2023-08-04 03:42:11 +0300

    1. added cells on storyboard

---
+++ To-Do Manager/View/Cells/TaskCell.swift
@@ -0,0 +1,23 @@
+//
+//  TaskCell.swift
+//  To-Do Manager
+//
+//  Created by 1234 on 04.08.2023.
+//
+
+import UIKit
+
+class TaskCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
